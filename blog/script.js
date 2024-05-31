901f21b8a856fd29b8457166e970f06a675738c8
lizzywooda <woo20081@byui.edu>
2024-05-31 17:59:03 -0600

    finished blog part 2

---
+++ blog/script.js
@@ -0,0 +1,21 @@
+const articlesContainer = document.getElementById('articles-container');
+
+articles.forEach(article => {
+    const articleElement = document.createElement('article'); 
+    articleElement.innerHTML = `
+        <div class="book-info">
+            <div class="book-meta">
+                <p>${article.date}</p>
+                <p>${article.ages}</p>
+                <p>${article.genre}</p>
+                <p>${article.stars}</p>
+            </div>
+            <div class="book-content">
+                <h2>${article.title}</h2>
+                <img src="${article.imgSrc}" alt="${article.imgAlt}">
+                <p>${article.description} <a href="#">Read More...</a></p>
+            </div>
+        </div>
+    `;
+    articlesContainer.appendChild(articleElement);
+});
