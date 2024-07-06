993add232ed08c0a11aabd25e97f8c69cc189b7a
lizzywooda <woo20081@byui.edu>
2024-07-06 17:33:25 -0600

    fixed cool pics

--- coolpics/coolpics.js
+++ coolpics/coolpics.js
@@ -1,57 +1,63 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const menuButton = document.querySelector('nav button');
-    const navList = document.querySelector('nav ul');
-    const galleryImages = document.querySelectorAll('.gallery img');
+const menuButton = document.querySelector('.menu-button');
+const navigation = document.querySelector('nav ul');
 
-    function toggleMenu() {
-        navList.classList.toggle('show');
-    }
+function closeMenu() {
+      menuButton.addEventListener('click', () => {
+      navigation.classList.toggle('close');
+   });
+}
 
-    function handleResize() {
-        if (window.innerWidth > 1000) {
-            navList.classList.remove('hide');
-            navList.classList.remove('show');
-        } else {
-            navList.classList.add('hide');
-        }
-    }
+closeMenu();
 
-    function viewerTemplate(pic, alt) {
-        return `<div class="viewer">
-          <button class="close-viewer">X</button>
-          <img src="${pic}" alt="${alt}">
-          </div>`;
-    }
 
-    function closeViewer() {
-        document.querySelector('.viewer').remove();
-    }
+function handleResize() {
+   if (window.innerWidth > 700) {
+      navigation.classList.remove('close');
+   }
+   else {
+      navigation.classList.add('close');
+   }
+}
 
-    function viewHandler(event) {
-        // create a variable to hold the element that was clicked on from event.target
-        const clickedElement = event.target;
-        // get the src attribute from that element and 'split' it on the "-"
-        const parts = clickedElement.src.split('-');
-        // construct the new image file name by adding "-full.jpeg" to the first part of the array from the previous step
-        const newSrc = parts[0] + '-full.jpeg';
-        // insert the viewerTemplate into the top of the body element
-        // (element.insertAdjacentHTML("afterbegin", htmltoinsert))
-        document.body.insertAdjacentHTML('afterbegin', viewerTemplate(newSrc, clickedElement.alt));
-        // add a listener to the close button (X) that calls a function called closeViewer when clicked
-        const closeButton = document.querySelector('.close-viewer');
-        closeButton.addEventListener('click', closeViewer);
-    }
+handleResize();
+window.addEventListener('resize', handleResize);
 
-    galleryImages.forEach(image => {
-        image.addEventListener('click', viewHandler);
-    });
+function viewerTemplate(picture, altText) {
+   return `
+    <div class="viewer">
+    <button class="close-viewer">❌</button>
+    <img src="${picture}" alt="${altText}">
+    </div>`
+}
 
-    
+function viewHandler(event) {
+	// create a variable to hold the element that was clicked on from event.target
+   const target = event.target;
+	// get the src attribute from that element and 'split' it on the "-"
+   const imgLink = target.src;
+   const split = imgLink.split('-');
+   const imgName = split[0];
 
-    menuButton.addEventListener('click', toggleMenu);
+	// construct the new image file name by adding "-full.jpeg" to the first part of the array from the previous step
+   const fullImage = 'norris-full.jpeg';
 
-    window.addEventListener('resize', handleResize);
+	// insert the viewerTemplate into the top of the body element
+   const body = document.body;
+	body.insertAdjacentHTML("afterbegin", viewerTemplate(fullImage, imgName));
 
-    handleResize();
+	// add a listener to the close button (X) that calls a function called closeViewer when clicked
 
-});
+   const closeViewerBtn = document.querySelector('.close-viewer');
+
+   function closeViewer() {
+      const viewer = document.querySelector('.viewer');
+      viewer.remove();
+   }
+
+   closeViewerBtn.addEventListener('click', () => {
+         closeViewer();
+   });
+}
+
+const images = document.querySelectorAll('.gallery img');
+images.forEach(image => image.addEventListener('click', viewHandler));
